be46a2d7f29a8fa5e1967e5e4d078ee00d690f5a
Fumihiro_Maejima <testerwatson1101@gmail.com>
2019-06-09 14:43:10 +0900

    modify page title.

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1">
-    <title>Laravel - osake</title>
+    <title>Shibuya - osake</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" type="text/css" href="{{ mix('css/app.css') }}">
     <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
