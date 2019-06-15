7b2d5f399c35d840ff0c20268f090eb68aeaca52
Fumihiro_Maejima <testerwatson1101@gmail.com>
2019-06-15 14:40:43 +0000

    delete no need line in controller.

--- app/Http/Controllers/MaintenancePageController.php
+++ app/Http/Controllers/MaintenancePageController.php
@@ -44,11 +44,6 @@ class MaintenancePageController extends Controller
 
         $responseData = json_decode($responseBody, true);
 
-
-        // チェック用
-        //dd($responseData);
-
-
         /*
         // 画面表示用データ(テスト用)の取得
         //$url = public_path() . '/data/testdata.xml';
@@ -96,41 +91,6 @@ class MaintenancePageController extends Controller
 
         // チェック用
         //dd($restaurantArray);
-        //dd($viewData);
-
-        /*
-        // 配列のkeyによってデータを振り分ける
-        foreach ($decodeData as $key => $restdata) {
-            switch ($key) {
-                case 'total_hit_count':
-                    $totalHitCount = $restdata;
-                    break;
-                case 'hit_per_page':
-                    $hitPerPage = $restdata;
-                    break;
-                case 'page_offset':
-                    $pageOffset = $restdata;
-                    break;
-                case 'rest':
-                    $restArray[] = $restdata;
-                    break;
-                default:
-                    break;
-            }
-        }
-
-        $viewData = null;
-        //$test = null;
-        //dd($restArray);
-        // 飲食店データをさらに個々の店舗のデータとしてまとめる
-        foreach ($restArray as $attributesData) {
-            $viewData = $attributesData;
-            $viewData = json_encode($viewData);
-        }
-        */
-        //dd($test);
-        //echo var_dump($totalHitCount);
-        //echo var_dump($viewData);
         //dd($viewData);
 
         return view('maintenance.index')->with('viewData', $viewData);
