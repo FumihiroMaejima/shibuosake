d420f88f2c9b90bbcf5057324be0235110c9010c
Fumihiro_Maejima <testerwatson1101@gmail.com>
2019-06-17 15:44:39 +0000

    meke some method for api execution.

--- app/Http/Controllers/MaintenancePageController.php
+++ app/Http/Controllers/MaintenancePageController.php
@@ -7,37 +7,11 @@ use \GuzzleHttp\Psr7;
 
 class MaintenancePageController extends Controller
 {
+    // トップページの表示
     public function index()
     {
-        //guzzle test
-        /**
-         * guzzleHttpClientによるAPI実行
-         * /RestSearchAPI/:レストラン検索API
-         * keyid:アクセスキー
-         * address:地名
-         * category_l:業態
-         **/
-        $baseUrl = 'https://api.gnavi.co.jp';
-        $path = '/RestSearchAPI/v3/?keyid=' . env('GURUNAVI_ACCESS_KEY') . '&address=渋谷&category_l=RSFST21000';
-        $client = new \GuzzleHttp\Client([
-            'base_uri' => $baseUrl,
-        ]);
-
-        $headers = [
-            'Origin'                    => 'https://google.com',
-            'Accept-Encoding'           => 'gzip, deflate, br',
-            'Accept-Language'           => 'ja,en-US;q=0.8,en;q=0.6',
-            'Upgrade-Insecure-Requests' => '1',
-            'Content-Type'              => 'application/json; charset=utf-8',
-        ];
-
-        $response = $client->request('GET', $path, [
-            'allow_redirects' => false,
-            'headers'         => $headers,
-        ]);
-        $responseBody = (string)$response->getBody();
-
-        $responseData = json_decode($responseBody, true);
+        // レストランデータの取得
+        $responseData = self::getRestaurantData();
 
         /* 整形用のデータを作成 */
         // 該当件数
@@ -74,9 +48,9 @@ class MaintenancePageController extends Controller
         return view('maintenance.index')->with('viewData', $viewData);
     }
 
-    public function apitest()
+    // レストラン検索APIの実行
+    public function getRestaurantData()
     {
-        //guzzle test
         /**
          * guzzleHttpClientによるAPI実行
          * /RestSearchAPI/:レストラン検索API
@@ -89,8 +63,6 @@ class MaintenancePageController extends Controller
         $client = new \GuzzleHttp\Client([
             'base_uri' => $baseUrl,
         ]);
-
-        //dd($path);
 
         $headers = [
             'Origin'                    => 'https://google.com',
@@ -108,7 +80,15 @@ class MaintenancePageController extends Controller
 
         $responseData = json_decode($responseBody, true);
 
-        dd($responseBody);
+        return $responseData;
+    }
+
+    public function apitest()
+    {
+        // APIの実行
+        $responseData = self::execApi();
+
+        dd($responseData);
 
         /* 整形用のデータを作成 */
         // 該当件数
@@ -146,6 +126,41 @@ class MaintenancePageController extends Controller
         //dd($restaurantArray);
         //dd($viewData);
 
-        return view('maintenance.index')->with('viewData', $viewData);
+        return view('maintenance.apitest')->with('viewData', $viewData);
+    }
+
+    // レストラン検索等のAPIの実行
+    public function execApi()
+    {
+        /**
+         * guzzleHttpClientによるAPI実行
+         * /RestSearchAPI/:レストラン検索API
+         * keyid:アクセスキー
+         * address:地名
+         * category_l:業態
+         **/
+        $baseUrl = 'https://api.gnavi.co.jp';
+        $path = '/RestSearchAPI/v3/?keyid=' . env('GURUNAVI_ACCESS_KEY') . '&address=渋谷&category_l=RSFST21000';
+        $client = new \GuzzleHttp\Client([
+            'base_uri' => $baseUrl,
+        ]);
+
+        $headers = [
+            'Origin'                    => 'https://google.com',
+            'Accept-Encoding'           => 'gzip, deflate, br',
+            'Accept-Language'           => 'ja,en-US;q=0.8,en;q=0.6',
+            'Upgrade-Insecure-Requests' => '1',
+            'Content-Type'              => 'application/json; charset=utf-8',
+        ];
+
+        $response = $client->request('GET', $path, [
+            'allow_redirects' => false,
+            'headers'         => $headers,
+        ]);
+        $responseBody = (string)$response->getBody();
+
+        $responseData = json_decode($responseBody, true);
+
+        return $responseData;
     }
 }
